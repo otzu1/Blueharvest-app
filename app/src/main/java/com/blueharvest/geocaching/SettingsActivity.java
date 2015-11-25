de0f1af66279b8cff0afefbb00b2917298a48bfc
jmb <jamie@whitepumpkin.com>
2015-11-25 02:15:46 -0500

    :honeybee: add found/favorite geocaches activities and settings activity

---
+++ app/src/main/java/com/blueharvest/geocaching/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.blueharvest.geocaching;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        // todo: this feature is forthcoming
+    }
+}
---
+++ app/src/main/java/com/blueharvest/geocaching/ViewFoundGeocachesActivity.java
@@ -0,0 +1,14 @@
+package com.blueharvest.geocaching;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewFoundGeocachesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_found_geocaches);
+        // todo: this feature is forthcoming
+    }
+}
